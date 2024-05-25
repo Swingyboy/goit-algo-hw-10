148b6c0c53977f58f673df8222ebbad6584b682e
serhii <sbezuhlyi@tektelic.com>
2024-05-25 08:35:39 +0300

    add plot.py

---
+++ plot.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from math import exp
+
+
+def f(x):
+    return exp(x)
+
+a = 0  # Нижня межа
+b = 2  # Верхня межа
+
+# Створення діапазону значень для x
+x = np.linspace(-0.5, 2.5, 400)
+y = [f(i) for i in x]
+
+# Створення графіка
+fig, ax = plt.subplots()
+
+# Малювання функції
+ax.plot(x, y, 'r', linewidth=2)
+
+# Заповнення області під кривою
+ix = np.linspace(a, b)
+iy = [f(i) for i in ix]
+ax.fill_between(ix, iy, color='gray', alpha=0.3)
+
+# Налаштування графіка
+ax.set_xlim([x[0], x[-1]])
+ax.set_ylim([0, max(y) + 0.1])
+ax.set_xlabel('x')
+ax.set_ylabel('f(x)')
+
+# Додавання меж інтегрування та назви графіка
+ax.axvline(x=a, color='gray', linestyle='--')
+ax.axvline(x=b, color='gray', linestyle='--')
+ax.set_title('Графік інтегрування f(x) = exp(x) від ' + str(a) + ' до ' + str(b))
+plt.grid()
+plt.show()
